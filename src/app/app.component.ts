c5d6dc60df0fe4814763d1e5182b07370bc1a4a0
rainiery valerio <rainiery.valerio@economia.gob.do>
2023-02-15 12:14:27 -0400

    Add popup geolocalization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BasemapType } from './esri-map/enum/BasemapType.enum';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'arcgis-test-map';
+  mapCenter = [-69.933491, 18.477923];
+  baseMapType = BasemapType.TopoVector;
+  nextBaseMapType = BasemapType.Satellite;
+  mapZoomLevel = 8;
+
+  mapLoadedEvent(status: boolean) {
+    console.log('The map has loaded: ' + status);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EsriMapComponent } from './esri-map/esri-map.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      EsriMapComponent
+   ],
   imports: [
     BrowserModule
   ],
